00268dc7c7a8e0442aeeefbc4ffda2d4081ecef0
nedhub <54211897+nedhub@users.noreply.github.com>
2021-07-26 21:12:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use((err, req, res, next) => {
   console.log(err.stack);
   console.log(err.name);
   console.log(err.code);
+  
 
   res.status(500).json({
     message: "Something went rely wrong",
